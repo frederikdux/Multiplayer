d3366d58fbb20696b99ef96b2d754b9140568180
Frederik Dux <freddinatorlp@gmail.com>
2023-10-25 22:34:07 +0200

    Elapsed Time und Enemy-Velocity

--- src/UI/GameManager.java
+++ src/UI/GameManager.java
@@ -15,6 +15,8 @@ public class GameManager {
     public final ArrayList<Enemy> enemies = new ArrayList<Enemy>();
     public final HashMap<Integer, Vector2f> enemysLastPositions = new HashMap<>();
     public final HashMap<Integer, Vector2f> enemysVelocity = new HashMap<>();
+    public final HashMap<Integer, Long> enemysRealLastUpdate = new HashMap<>();
+    public final HashMap<Integer, Long> enemysLastUpdate = new HashMap<>();
 
     long lastUpdate = 0;
     long newUpdate = 0;
@@ -39,7 +41,7 @@ public class GameManager {
         });
 
         Thread gameUpdater = new Thread(() -> {
-
+            gameUpdate();
         });
 
         backend.start();
@@ -49,27 +51,36 @@ public class GameManager {
 
     public void gameUpdate(){
         for (Enemy enemy : enemies){
+            if(enemysLastUpdate.get(enemy.id) - enemysRealLastUpdate.get(enemy.id) > 100000000) {
+                Vector2f lastPosition = enemysLastPositions.get(enemy.id);
+                Vector2f velocity = enemysVelocity.get(enemy.id);
+                Long lastUpdate = enemysLastUpdate.get(enemy.id);
+
+                float elapsed = (float) (System.nanoTime() - lastUpdate) / 100000000;
+                enemy.setPos(new Vector2f(lastPosition.x + velocity.x * elapsed, lastPosition.y + velocity.y * elapsed));
+                enemysLastUpdate.replace(enemy.id, System.nanoTime());
+            }
+            else{
+                enemy.setPos(enemysLastPositions.get(enemy.id));
+            }
         }
     }
 
     public void calculateVelocity(int id, Vector2f pos){
-        if(enemysLastPositions.containsKey(id)) {
-            newUpdate = System.nanoTime();
-            elapsedTime = (newUpdate - lastUpdate) / 1000;
-            lastUpdate = newUpdate;
-            float elapsedTimeSec = ((float) elapsedTime) / 100000;
-            System.out.println(elapsedTimeSec);
+
+        enemysLastUpdate.replace(id, System.nanoTime());
+        newUpdate = System.nanoTime();
+        elapsedTime = (newUpdate - lastUpdate) / 1000;
+        lastUpdate = newUpdate;
+        float elapsedTimeSec = ((float) elapsedTime) / 100000;
+        System.out.println(elapsedTimeSec);
 
 
-            Vector2f lastPosition = enemysLastPositions.get(id);
-            Vector2f velocity = new Vector2f((pos.x - lastPosition.x) * elapsedTimeSec, (pos.y - lastPosition.y) * elapsedTimeSec);
-            enemysVelocity.replace(id, velocity);
-            System.out.println("Velocity: " + velocity.x + " " + velocity.y);
-            enemysLastPositions.replace(id, pos);
-        }
-        else{
-            enemysLastPositions.put(id, pos);
-        }
+        Vector2f lastPosition = enemysLastPositions.get(id);
+        Vector2f velocity = new Vector2f((pos.x - lastPosition.x) * elapsedTimeSec, (pos.y - lastPosition.y) * elapsedTimeSec);
+        enemysVelocity.replace(id, velocity);
+        System.out.println("Velocity: " + velocity.x + " " + velocity.y);
+        enemysLastPositions.replace(id, pos);
     }
 
     public void updateEnemies(){
@@ -116,6 +127,10 @@ public class GameManager {
     public void registerNewEnemy(Player player){
         Enemy enemy = new Enemy(player.getId(), player.getClientName(), player.getPos(), player.getRot());
         enemies.add(enemy);
+        enemysLastPositions.put(enemy.id, enemy.getPos());
+        enemysVelocity.put(enemy.id, new Vector2f(0, 0));
+        enemysLastUpdate.put(enemy.id, System.nanoTime());
+        enemysRealLastUpdate.put(enemy.id, System.nanoTime());
     }
     public void logoutEnemy(int id){
         //client.receivedGameInformations.removePlayerByID(id);
